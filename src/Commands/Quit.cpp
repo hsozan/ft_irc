fa38175c31f9428baa5f020b8aca9fb6c7ec4358
kratoshakan <hakansozan.inci@gmail.com>
2024-02-09 13:08:11 +0300

    Add commandParts parameter to quit function

--- src/Commands/Quit.cpp
+++ src/Commands/Quit.cpp
@@ -2,6 +2,7 @@
 
 void Quit::quit(Client *client, vector<string> commandParts, Server *srv)
 {
+    (void)commandParts;
     client->sendMessage(RPL_QUIT(client->getPrefix(), "Leaving server..."));
     srv->removeClientFromAllChannels(client);
     srv->clientDisconnect(client->getClientSocketFD());
